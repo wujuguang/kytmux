5e48b80fa375d63ef3652c0667578060e436a63e
wujuguang <1154545932@qq.com>
2015-07-12 12:30:08 +0800

    kytmux config initialization

---
+++ readme.md
@@ -0,0 +1,24 @@
+# kyTmux
+
+My tmux development environment Settings.
+
+# 安装
+
+### 下载代码
+- git clone https://github.com/wujuguang/kytmux ~/kytmux    
+- git clone https://github.com/tmux-plugins/tpm.git ~/kytmux/tmux/plugins/tpm
+
+### 创建链接
+- ln -s ~/kytmux/tmux ~/.tmux    
+- ln -s ~/kytmux/tmux.conf ~/.tmux.conf    
+- ln -s ~/kytmux/tmux-powerline ~/.tmux-powerline
+
+### 安装插件    
+Open tmux and hit `prefix + I` to fetch all plugins.
+
+# 插件
+
+所有插件的安装通过 [`tpm`](https://github.com/tmux-plugins/tpm).
+
+# 一键安装
+下载执行 setup.sh: wget https://raw.githubusercontent.com/wujuguang/kytmux/master/setup.sh && sh setup.sh
---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/wujuguang/kytmux.git ~/kytmux/
+git clone https://github.com/erikw/tmux-powerline ~/kytmux/tmux-powerline/
+git clone https://github.com/tmux-plugins/tpm.git ~/kytmux/tmux/plugins/tpm/
+
+mv ~/.tmux ~/.tmux.bck
+mv ~/.tmux.conf ~/.tmux.conf.bck
+mv ~/.tmux-powerline ~/.tmux-powerline.bck
+
+ln -s ~/kytmux/tmux ~/.tmux
+ln -s ~/kytmux/tmux.conf ~/.tmux.conf
+ln -s ~/kytmux/tmux-powerline ~/.tmux-powerline
+
+# 安装tmux-powerline依赖与字体
+pip install powerline-status
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+
+mkdir -p ~/.fonts/ ~/.config/fontconfig/conf.d/
+mv PowerlineSymbols.otf ~/.fonts/
+mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+fc-cache -vf ~/.fonts/
+
+# 自定义状态栏
+~/.tmux-powerlinerc/generate_rc.sh
+cp ~/.tmux-powerline/themes/default.sh mine.sh
+mv ~/.tmux-powerlinerc.default ~/.tmux-powerlinerc
+# 修改~/.tmux-powerline/themes/mine.sh ~/.tmux-powerlinerc
+
+# 安装tpm下插件
+tmux new-session -d
+tmux run "~/kytmux/tmux/plugins/tpm/scripts/install_plugins.sh"
+tmux kill-session
